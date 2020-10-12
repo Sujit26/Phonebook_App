b7d1c119341744ed92dc889786e50240185c5481
Sujit Jaiwaliya <2017csb1115@iitrpr.ac.in>
2020-10-13 02:10:40 +0530

    create new ContactList

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React from 'react';
 import './App.css';
+import ContactList from './ContactList.js'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        // name: '',
+        // phone_number:'',
+        // address:'',
+        text: '',
+        key: '',
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  };
 
-class App extends React.Component{
-  render(){
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem)
+    if (newItem.text !== "") {
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: {
+          key: '',
+          text: ''
+        }
+      })
+    }
+
+
+  }
+  render() {
     return (
-      <div className = "className">
+      <div className="className">
         <header>
-        <form id = "phoneBookApp">
-          <input type ="text" placeholder= "Enter Text"/>
-          <button type= 'submit' placeholder>Add</button>
-        </form>
-      </header>
+          <form id="phoneBookApp" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter Text"
+              value={this.currentItem}
+              onChange={this.handleInput}
+            />
+            <button type='submit' placeholder>Add</button>
+          </form>
+        </header>
+        <ContactList></ContactList>
       </div>
     );
-  
+
   }
 }
 
---
+++ src/ContactList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ContactList.css'
+
+
+function ContactList(props){
+    return (
+        <h1>
+            From ContactList.js
+        </h1>
+    )
+}
+export default ContactList;
