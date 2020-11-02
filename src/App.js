45714f50d4739ed005e6a8431ec2e49cb9764578
Sujit Jaiwaliya <2017csb1115@iitrpr.ac.in>
2020-11-02 23:00:30 +0530

    todo template completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import ContactList from './ContactList.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+library.add(faTrash)
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -16,6 +19,8 @@ class App extends React.Component {
     }
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
   }
   handleInput(e) {
     this.setState({
@@ -40,10 +45,32 @@ class App extends React.Component {
         }
       })
     }
+  };
+  
+  deleteItem(key){
+    const filteredItems= this.state.items.filter(item =>
+      item.key!==key);
+    this.setState({
+      items: filteredItems
+    })
+  };
 
+  setUpdate(text,key){
+    console.log("items:"+this.state.items);
+    const items = this.state.items;
+    items.map(item=>{      
+      if(item.key===key){
+        console.log(item.key +"    "+key)
+        item.text= text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+  };
 
-  }
   render() {
+    
     return (
       <div className="className">
         <header>
@@ -55,7 +82,11 @@ class App extends React.Component {
             <button type='submit' placeholder>Add</button>
           </form>
         </header>
-        <ContactList></ContactList>
+        <ContactList
+          items={this.state.items}
+          deleteItem = {this.deleteItem}
+          setUpdate = {this.setUpdate}
+        ></ContactList>
       </div>
     );
 
--- src/ContactList.js
+++ src/ContactList.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import './ContactList.css'
+import React from "react";
+import "./ContactList.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import FlipMove from "react-flip-move";
 
-
-function ContactList(props){
-    return (
-        <h1>
-            From ContactList.js
-        </h1>
-    )
+function ContactList(props) {
+    const items = props.items;
+    // const ListItems = items
+    const ListItems = items.map((item) => {
+        return (
+            <div className="list" key="item.key">
+                <p>
+                    <input
+                        type="text"
+                        id={item.key}
+                        value={item.text}
+                        onChange={(e) => {
+                            props.setUpdate(e.target.value, item.key);
+                        }}
+                    />{" "}
+                    <span>
+                        <FontAwesomeIcon
+                            className="facions"
+                            icon="trash"
+                            onClick={() => props.deleteItem(item.key)}
+                        />{" "}
+                    </span>
+                </p>
+            </div>
+        );
+    });
+    return <div duration={300} easing="ease-in-out"> 
+        <FlipMove >  
+        {ListItems} 
+        </FlipMove>
+        </div>;
 }
 export default ContactList;
